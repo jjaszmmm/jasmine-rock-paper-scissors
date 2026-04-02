ec2512232b9ea330e1ecabd93ab13367ead79a82
Jasmine Jimenez <jj11765252gmail.com>
2026-04-02 11:01:25 +0800

    Add play function and display choices

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 let playerScore = 0;
 let computerScore = 0;
+
+function play(playerChoice){
+
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    document.getElementById("playerChoice").innerText =
+        "Player chose: " + playerChoice;
+
+    document.getElementById("computerChoice").innerText =
+        "Computer chose: " + computerChoice;
+
+}
